517ff7a0bf4ab5cdf38037c6be4db7033280099b
mingern789 <63950500+mingern789@users.noreply.github.com>
2022-03-08 13:14:06 +0800

    lightly styled buttons

--- src/App.vue
+++ src/App.vue
@@ -24,4 +24,25 @@ import CustomerPanel from './components/CustomerPanel.vue'
   color: #2c3e50;
   margin-top: 60px;
 }
+
+button {
+  background-color: white; /* Green */
+  border: none;
+  color: black;
+  padding: 16px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+  border: 2px solid #4CAF50;
+  border-radius: 4px;
+}
+
+button:hover {
+  background-color: #4CAF50;
+  color: white;
+}
 </style>
