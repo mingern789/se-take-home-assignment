d33097072f44c87a132cc17bae83fb03f80d78bb
mingern789 <63950500+mingern789@users.noreply.github.com>
2022-03-08 13:47:13 +0800

    Revert "vite config changes"
    This reverts commit 3a4d1c18361009e867ec3cde5f9c3589e6bc0989.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/se-take-home-assignment/',
+  base: '/se-take-home-assignment',
   plugins: [vue()]
 })
